7dcbefd014fc80ddda892d01d9326add20d7797d
bthalheim <ben@bthal.com>
2023-12-02 12:40:48 -0500

    Initial commit for advent template

---
+++ src/cmake_advent_template/README.md
@@ -0,0 +1,9 @@
+CMake Advent Of Code Template
+=========================================================
+A template for advent of code challanges using cmake. 
+
+Yes, its overkill; but I wanted to help retain muscle memory with cmake.
+
+
+
+
---
+++ src/cmake_advent_template/src/main.cpp
@@ -0,0 +1,17 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long // Use int instead of long long for simplicity
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    // Your code here
+
+    return 0;
+}
+
+
