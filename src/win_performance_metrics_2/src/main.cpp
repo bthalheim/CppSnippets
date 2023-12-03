84d4fa4ba6e92e4c36c287772bc1c5745f5c7609
bthalheim <ben@bthal.com>
2023-12-03 08:20:13 -0500

    Adding second test of perf metrics

---
+++ src/win_performance_metrics_2/src/main.cpp
@@ -0,0 +1,37 @@
+
+
+#include <iostream>
+#include <windows.h>
+#include <pdh.h>
+
+#pragma comment(lib, "pdh.lib")
+
+int main() {
+    PDH_HQUERY query;
+    PDH_HCOUNTER counter;
+
+    // Initialize PDH
+    PdhOpenQuery(nullptr, 0, &query);
+    PdhAddCounter(query, "\\Processor(_Total)\\C1 Transitions/sec", 0, &counter);
+    PdhCollectQueryData(query);
+
+    // Query and display CPU usage every second
+    for (int i = 0; i < 10; ++i) {
+        Sleep(1000);
+
+        PDH_FMT_COUNTERVALUE value;
+        PdhCollectQueryData(query);
+        PdhGetFormattedCounterValue(counter, PDH_FMT_DOUBLE, nullptr, &value);
+
+        std::cout << "C1 Transitions/sec: " << value.doubleValue << "%" << std::endl;
+    }
+
+    // Close PDH
+    PdhCloseQuery(query);
+
+    return 0;
+}
+
+
+
+
